0550c68ff85f157446cdfc9138adf81cae2d38d8
anubhav <anubhav@sportskeeda.com>
2024-01-18 16:51:38 +0530

    udpates to CharactersRepository

--- data/src/main/java/com/asraven/start_wars/data/CharactersRepository.kt
+++ data/src/main/java/com/asraven/start_wars/data/CharactersRepository.kt
@@ -1,12 +1,13 @@
 package com.asraven.start_wars.data
 
+import androidx.paging.PagingData
 import com.asraven.star_wars.model.CharacterStarWars
 import kotlinx.coroutines.flow.Flow
 
 
 
-interface CharactersRepository {
+interface CharactersRepository: Syncable {
 
-    fun getCharactersResources(
-    ) : Flow<List<CharacterStarWars>>
+    fun getCharactersResources(pageNumber: Int)
+     : Flow<PagingData<CharacterStarWars>>
 }

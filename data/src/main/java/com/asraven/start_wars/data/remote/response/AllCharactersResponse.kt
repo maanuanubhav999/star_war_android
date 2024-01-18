b2b215f9ae0baf0c15fc0f04a7fe4171aa23e4d2
anubhav <anubhav@sportskeeda.com>
2024-01-18 16:51:03 +0530

    all characters response data class

---
+++ data/src/main/java/com/asraven/start_wars/data/remote/response/AllCharactersResponse.kt
@@ -0,0 +1,29 @@
+package com.asraven.start_wars.data.remote.response
+
+import com.asraven.star_wars.model.CharacterStarWars
+
+data class AllCharactersResponse(
+    val count: Int,
+    val next: String,
+    val previous: String?,
+    val results: List<CharacterStarWars>
+)
+//
+//data class Result(
+//    val birth_year: String,
+//    val created: String,
+//    val edited: String,
+//    val eye_color: String,
+//    val films: List<String>,
+//    val gender: String,
+//    val hair_color: String,
+//    val height: String,
+//    val homeworld: String,
+//    val mass: String,
+//    val name: String,
+//    val skin_color: String,
+//    val species: List<String>,
+//    val starships: List<String>,
+//    val url: String,
+//    val vehicles: List<String>
+//)

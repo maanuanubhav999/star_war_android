d3dab774c658f20fcdb8bc6376b89d39402aa7c8
anubhav <anubhav@sportskeeda.com>
2024-01-20 22:40:36 +0530

    setup for dependecy injection

--- app/src/main/java/com/asraven/star_wars/MainActivity.kt
+++ app/src/main/java/com/asraven/star_wars/MainActivity.kt
@@ -10,8 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.asraven.star_wars.home.navigation.homeGenerationGraph
+import com.asraven.star_wars.home.navigation.homeNavigationPattern
 import com.asraven.star_wars.ui.theme.Star_warsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    NavHost(
+                        navController =  rememberNavController(),
+                        startDestination = homeNavigationPattern,
+                    ) {
+                        homeGenerationGraph()
+                    }
                 }
             }
         }
